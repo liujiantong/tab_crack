f8914fd9696c3f10d4fb3f7466a52bbb1a0031d6
刘涛1 <tao.liu1@ikang.com>
2016-12-22 21:05:19 +0800

    refined util.py

--- utils.py
+++ utils.py
@@ -7,6 +7,10 @@ import mysql.connector
 
 
 mail_server = 'mail.ikang.com'
+mysql_url = '127.0.0.1'
+mysql_user = 'scott'
+mysql_passwd = 'tiger'
+mysql_db = 'employees'
 
 
 def email_login(email, passwd, logger=None):
@@ -21,37 +25,35 @@ def email_login(email, passwd, logger=None):
                 break
             else:
                 # print 'connection %d failed' % i
-                time.sleep(1)
+                time.sleep(1 + 0.5*i)
         except Exception as e:
-            msg = 'connect to %s error: %s'.format(mail_server, e)
+            msg = 'connect to `{}` error: `{}`'.format(mail_server, e)
             if logger:
                 logger.warning(msg)
             else:
                 print msg
     if not connected:
         return False
+
     code, _ = smtp.login(email, passwd)
     # print 'login code:', code
-
-    if int(code/100) == 2:
-        return True
-    return False
+    return int(code/100) == 2
 
 
 def token_by_email(email):
-    cnx = mysql.connector.connect(user='scott', password='tiger',
-                                  host='127.0.0.1',
-                                  database='employees')
+    cnx = mysql.connector.connect(user=mysql_user, password=mysql_passwd, host=mysql_url, database=mysql_db)
     try:
         cursor = cnx.cursor()
-        cursor.execute("select token from tab_user where email=%s", email)
+        cursor.execute("select token from tab_user where email=%s", (email,))
         result = cursor.fetchone()
-        print result[0]
+        return None if result is None else result[0]
     finally:
         cnx.close()
 
 
 if __name__ == '__main__':
-    # assert email_login("auth@ikang.com", "Ikang@$1jk!")
-    assert token_by_email('email@ikang.com') == 'xxxx'
+    st = time.time()
+    assert email_login("auth@ikang.com", "Ikang@$1jk!")
+    # assert token_by_email('email@ikang.com') == 'xxxx'
+    print 'I took %d ms to login' % int((time.time()-st) * 1000)
 
