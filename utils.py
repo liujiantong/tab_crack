90effc46543d62b9d779c98be13b220fa6f5e0be
刘涛1 <tao.liu1@ikang.com>
2016-12-23 13:53:54 +0800

    change db to production env

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-#!/anaconda/bin/python
+#!/home/tao.liu1/anaconda/bin/python
 # coding: utf8
 
 import time
@@ -7,10 +7,10 @@ import mysql.connector
 
 
 mail_server = 'mail.ikang.com'
-mysql_url = '127.0.0.1'
-mysql_user = 'scott'
-mysql_passwd = 'tiger'
-mysql_db = 'employees'
+mysql_url = '192.168.0.173'
+mysql_user = 'u_user_view'
+mysql_passwd = 'Fb7HM&TZ%Npd'
+mysql_db = 'etl_ctl'
 
 
 def email_login(email, passwd, logger=None):
@@ -45,7 +45,10 @@ def token_by_email(email):
     cnx = mysql.connector.connect(user=mysql_user, password=mysql_passwd, host=mysql_url, database=mysql_db)
     try:
         cursor = cnx.cursor()
-        cursor.execute("select token from tab_user where email=%s", (email,))
+        cursor.execute("select a.token from etl_ctl.t_user a "
+                       "where a.email_address=lower(%s) and a.user_type='1' "
+                       "and a.is_enabled='1'", (email,))
+        # cursor.execute("select username from users where email=%s", (email,))
         result = cursor.fetchone()
         return None if result is None else result[0]
     finally:
@@ -55,6 +58,6 @@ def token_by_email(email):
 if __name__ == '__main__':
     st = time.time()
     assert email_login("auth@ikang.com", "Ikang@$1jk!")
-    # assert token_by_email('email@ikang.com') == 'xxxx'
+    assert token_by_email('sandy.cheng@ikang.com') == 'YR2Sn6QLqGkb'
     print 'I took %d ms to login' % int((time.time()-st) * 1000)
 
