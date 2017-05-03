4081797ebbefed93444541580f7589e3996919b8
刘涛1 <tao.liu1@ikang.com>
2017-05-03 17:05:49 +0800

    change username/password in production env

--- conf.py
+++ conf.py
@@ -8,12 +8,12 @@ mail_server = 'mail.ikang.com'
 
 mysql_host = '192.168.0.173'
 mysql_user = 'u_user_view'
-mysql_passwd = 'Fb7HM&TZ%Npd'
+mysql_passwd = '*********'
 mysql_db = 'etl_ctl'
 mysql_pool_size = 5
 
 tab_username = "common_user"
-tab_password = 'commonpass'
+tab_password = '***********'
 
 dbconfig = {
     'host': mysql_host,
@@ -30,4 +30,3 @@ redis_conf = {
 
 mail_relay_url = 'http://101.254.182.87:8777/mail_login'
 mail_relay_key = 'f693892e-2faf-11e7-9673-acbc32acde63'
-
